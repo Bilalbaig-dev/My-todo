a267229c3f5dc716945690c84d99c09194fb2dab
bilal baig <bilalbaig.dev@gmail.com>
2026-02-19 00:10:45 -0800

    Final update

--- todo.js
+++ todo.js
@@ -1,39 +1,45 @@
-const todoElement=document.querySelector('.input-todo');
-const dateElement=document.querySelector('.input-date');
-let display=document.querySelector('.js-display');
-let dataBase=[];
+const todoElement = document.querySelector('.input-todo');
+const dateElement = document.querySelector('.input-date');
+let display = document.querySelector('.js-display');
 
-        
-    function getData(){
-      if(todoElement.value===''||dateElement.value===''){
-         return;
-      }
-          let object={
-            name:todoElement.value,
-            date:dateElement.value
-        }
-        dataBase.push(object); 
-      render();
-    }
-  
-  function render(){
-       display.innerHTML='';
-      for(let i=0;i<dataBase.length;i++){
-      display.innerHTML+=`
-    <div class="todo-container">
-      <h4>${dataBase[i].name}</h4>
-      <h4>${dataBase[i].date}</h4>
-      <button id="red" class="btn"
-     onclick="
-      deleteBtn(${i})
-      "
-      >Delete</button>
-    </div>`;
-    }
+let dataBase = JSON.parse(localStorage.getItem('todos')) || [];
+
+render();
+
+function getData() {
+  if (todoElement.value === '' || dateElement.value === '') {
+    return;
   }
-  function deleteBtn(index){
-   dataBase.splice(index,1);
-   render();
+
+  let object = {
+    name: todoElement.value,
+    date: dateElement.value
+  };
+
+  dataBase.push(object);
+  localStorage.setItem('todos', JSON.stringify(dataBase));
+  todoElement.value = '';
+  dateElement.value = '';
+  render();
+}
+
+function render() {
+  display.innerHTML = '';
+  for (let i = 0; i < dataBase.length; i++) {
+    display.innerHTML += `
+      <div class="todo-container">
+        <h4>${dataBase[i].name}</h4>
+        <h4>${dataBase[i].date}</h4>
+        <button id="red" class="btn" onclick="deleteBtn(${i})">Delete</button>
+      </div>
+    `;
   }
+}
+
+function deleteBtn(index) {
+  dataBase.splice(index, 1);
+  localStorage.setItem('todos', JSON.stringify(dataBase));
+  render();
+}
 
  
